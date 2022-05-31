ece01d6d18ac9e409d9fd44f310ec5abc117cdf8
gedi <gedi@gagna.io>
2022-05-31 11:13:10 +0200

    ignore any

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,14 +1,17 @@
 declare module "*.svg" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
 
 declare module "*.css" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
 
 declare module "*.jpg" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const content: any;
   export default content;
 }
