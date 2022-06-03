d8de1cab1859032f879de29044970c8267f49008
gedi <gedi@gagna.io>
2022-06-03 10:21:43 +0200

    added header functionality

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,16 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import MainLogo from "../Icons/MainLogo/MainLogo";
 import Wrapper from "../Layout/Wrapper";
 
-const StyledHeader = styled.header`
+interface StyledHeaderProps {
+  showBackground: boolean;
+}
+
+const StyledHeader = styled.header<StyledHeaderProps>`
     width: 100%;
     z-index: 10;
     position: fixed;
+    transition: .2s;
+    background: ${p => p.showBackground ? 'rgba(0, 0, 0, .8)' : 'none'};
 `;
 
 const StyledWrapper = styled(Wrapper)`
-    padding: 1.5em 1em;
+    padding: 1em;
     width: 100%;
     display: flex;
     align-items: center;
@@ -28,8 +35,21 @@ const StyledMenuItem = styled(Link)`
 `
 
 const Header = () => {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  },[]);
+
+  const onScroll = () => {
+    setIsScrolling(!isScrolling && window.scrollY > 0);
+  };
+
   return (
-    <StyledHeader>
+    <StyledHeader showBackground={isScrolling}>
       <StyledWrapper>
         <MainLogo theme='white' />
         <div>
